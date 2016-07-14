9a057ae76b1c27b1ba5b079f0f17b400489062af
TempuSFatumA <strukovdim-a@yandex.ru>
2016-07-14 20:37:38 +0300

    Проект переработан на 3D-вид со спрайтами. Улучшена механика анимации рыцаря и добавлены новые виды анимации. Добавлен счётчик попаданий по скелету и подсказки.

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -1,52 +1,132 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class KnightController : MonoBehaviour {
+public class KnightController : MonoBehaviour
+{
+    public float baseSpeed;
+    public GameObject Representation;
+    public LayerMask whatIsEnemy;
+    public Text score;
+    public bool gotAttackFinished;
 
     Animator anim;
-    public float speed;
+    bool gotAttackChecked;
+    int successfullAttacksCount;
+    Vector3 macePosition;
+    float xMove;
+    float zMove;
 
-    // Use this for initialization
-    void Start () {
-        anim = GetComponent<Animator>();
-	}
-    
-	void FixedUpdate ()
+    void Start()
     {
-        float yMove = Input.GetAxis("Vertical");
-        float xMove = Input.GetAxis("Horizontal");
-
-        if (yMove > 0) 
-        {
-            SetDirection(true, false, false, false);
-            anim.SetFloat("movingState", 1.0f);
-        } else if (yMove < 0)
-        {
-            SetDirection(false, false, false, true);
-            anim.SetFloat("movingState", 1.0f);
-        } else if (xMove > 0)
-        {
-            SetDirection(false, false, true, false);
-            anim.SetFloat("movingState", 1.0f);
-        } else if (xMove < 0)
-        {
-            SetDirection(false, true, false, false);
-            anim.SetFloat("movingState", 1.0f);
-        } else
-        {
-            anim.SetBool("isWalking", false);
-            anim.SetFloat("movingState", 0.0f);
-        }
-        transform.position = new Vector3(transform.position.x + xMove * speed / 100,
-                                            transform.position.y + yMove * speed / 100);
-	}
-
-    void SetDirection(bool top, bool left, bool right, bool bottom)
-    {
-        anim.SetBool("isFacingTop", top);
-        anim.SetBool("isFacingLeft", left);
-        anim.SetBool("isFacingRight", right);
-        anim.SetBool("isFacingBottom", bottom);
+        gotAttackChecked = false;
+        successfullAttacksCount = 0;
+        score.text = "Skulls: " + successfullAttacksCount.ToString();
+        anim = Representation.GetComponent<Animator>();
+        SetFacingDirection(Constants.Bottom);
+        SetActionType(Constants.Idle);
     }
 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            SetActionType(Constants.Attack);
+            if (gotAttackFinished) {
+                if (!gotAttackChecked) {
+                    Collider[] hitColliders = Physics.OverlapSphere(macePosition, 0.25f, whatIsEnemy);
+                    foreach (Collider hitCollider in hitColliders) {
+                        if (!hitCollider.isTrigger) {
+                            successfullAttacksCount++;
+                            score.text = "Skulls: " + successfullAttacksCount.ToString();
+                        }
+                    }
+                    gotAttackChecked = true;
+                }
+            } else {
+                gotAttackChecked = false;
+            }
+        } else {
+
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                if (Input.GetKey(KeyCode.LeftArrow)) {
+                    zMove = 1.0f;
+                    xMove = -1.0f;
+                } else if (Input.GetKey(KeyCode.RightArrow)) {
+                    zMove = 1.0f;
+                    xMove = 1.0f;
+                } else if (Input.GetKey(KeyCode.DownArrow)) {
+                    zMove = 0.0f;
+                    xMove = 0.0f;
+                } else {
+                    zMove = 1.0f;
+                    xMove = 0.0f;
+                }
+            } else if (Input.GetKey(KeyCode.LeftArrow)) {
+                if (Input.GetKey(KeyCode.RightArrow)) {
+                    zMove = 0.0f;
+                    xMove = 0.0f;
+                } else if (Input.GetKey(KeyCode.DownArrow)) {
+                    zMove = -1.0f;
+                    xMove = -1.0f;
+                } else {
+                    zMove = 0.0f;
+                    xMove = -1.0f;
+                }
+            } else if (Input.GetKey(KeyCode.RightArrow)) {
+                if (Input.GetKey(KeyCode.DownArrow)) {
+                    zMove = -1.0f;
+                    xMove = 1.0f;
+                } else {
+                    zMove = 0.0f;
+                    xMove = 1.0f;
+                }
+            } else if (Input.GetKey(KeyCode.DownArrow)) {
+                zMove = -1.0f;
+                xMove = 0.0f;
+            } else {
+                zMove = 0.0f;
+                xMove = 0.0f;
+            }
+
+            if (zMove > 0) {
+                SetFacingDirection(Constants.Top);
+                SetActionType(Constants.Walk);
+            } else if (zMove < 0) {
+                SetFacingDirection(Constants.Bottom);
+                SetActionType(Constants.Walk);
+            } else if (xMove > 0) {
+                SetFacingDirection(Constants.Right);
+                SetActionType(Constants.Walk);
+            } else if (xMove < 0) {
+                SetFacingDirection(Constants.Left);
+                SetActionType(Constants.Walk);
+            } else {
+                SetActionType(Constants.Idle);
+            }
+            transform.position = new Vector3(transform.position.x + xMove * baseSpeed / 100,
+                                                transform.position.y,
+                                                    transform.position.z + zMove * baseSpeed / 100);
+        }
+    }
+
+    void SetActionType(float _State)
+    {
+        anim.SetFloat("actionType", _State);
+    }
+    void SetFacingDirection(float _Direction)
+    {
+        anim.SetFloat("facingDirection", _Direction);
+        if (_Direction == Constants.Top) {
+            macePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.15f);
+        } else if (_Direction == Constants.Left) {
+            macePosition = new Vector3(transform.position.x - 1.15f, transform.position.y, transform.position.z);
+        } else if (_Direction == Constants.Right) {
+            macePosition = new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z);
+        } else if (_Direction == Constants.Bottom) {
+            macePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1.15f);
+        } else {
+            macePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+    float GetFacingDirection() { return anim.GetFloat("facingDirection"); }
 }
--- Assets/Scripts/SkeletonAI.cs
+++ Assets/Scripts/SkeletonAI.cs
@@ -2,76 +2,43 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class SkeletonAI : MonoBehaviour {
-	
-	Animator anim;
-	//public Text xCor;
-	//public Text yCor;
-	SpriteRenderer sr;
-	Collider2D[] colliders2D;
-	// Use this for initialization
-	void Start () {
+public class SkeletonAI : MonoBehaviour
+{
 
-		anim = GetComponent<Animator> ();
-		sr = GetComponent<SpriteRenderer> ();
+    public GameObject Representation;
 
-	}
+    Animator anim;
 
-	void OnTriggerStay2D(Collider2D sight)
-	{
-		if (sight.gameObject.tag == "Player") 
-		{		
-			float xDif = transform.position.x - sight.gameObject.transform.position.x;
-			float yDif = transform.position.y - sight.gameObject.transform.position.y;
-			//xCor.text=string.Format("{0:f}", xDif);
-			//yCor.text=string.Format("{0:f}", yDif);
-			colliders2D = gameObject.GetComponents<CircleCollider2D>();
-			foreach (Collider2D collider2D in colliders2D) {
-				if (collider2D.isTrigger) {
-					if (collider2D.transform.position.y + transform.position.y >
-					   sight.transform.position.y + sight.gameObject.transform.position.y) {
-						sr.sortingOrder = -1;
-					} else {
-						sr.sortingOrder = 1;
-					}
-				}
-			}
-			if (Mathf.Abs (xDif) > Mathf.Abs (yDif)) {
-				if (xDif > 0) {
-					SetFacingDirection (false, true, false, false);
-				} else {	
-					SetFacingDirection (false, false, true, false);
-				} 
-		}
-			else
-			{
-				if (yDif>0)
-				{
-					SetFacingDirection (false, false, false, true);
-				}
-				else
-				{
-					SetFacingDirection (true, false, false, false);
-				}
-			}
+    void Start()
+    {
+        anim = Representation.GetComponent<Animator>();
+        //SetFacingDirection(Constants.Bottom);
+    }
 
-			anim.SetBool ("gotPlayerSpotted", true);
+    void OnTriggerStay(Collider intruder)
+    {
+        if (intruder.gameObject.tag == "Player") {
+            float xDif = transform.position.x - intruder.gameObject.transform.position.x;
+            float zDif = transform.position.z - intruder.gameObject.transform.position.z;
+            if (Mathf.Abs(xDif) > Mathf.Abs(zDif)) {
+                if (xDif > 0) {
+                    SetFacingDirection(Constants.Left);
+                } else {
+                    SetFacingDirection(Constants.Right);
+                }
+            } else {
+                if (zDif > 0) {
+                    SetFacingDirection(Constants.Bottom);
+                } else {
+                    SetFacingDirection(Constants.Top);
+                }
+            }
+            anim.SetBool("gotPlayerSpotted", true);
+        } else {
+            anim.SetBool("gotPlayerSpotted", false);
+        }
+    }
 
-		}
-	}
-	void Update()
-	{
-		
-	}
-
-		void SetFacingDirection(bool top, bool left, bool right, bool bottom)
-		{
-			anim.SetBool("spottedPlayer", false);
-			anim.SetBool("isFacingTop", top);
-			anim.SetBool("isFacingLeft", left);
-			anim.SetBool("isFacingRight", right);
-			anim.SetBool("isFacingBottom", bottom);
-		}
-	// Update is called once per frame
-
+    void SetActionType(float _State) { anim.SetFloat("actionType", _State); }
+    void SetFacingDirection(float _Direction) { anim.SetFloat("facingDirection", _Direction); }
 }
